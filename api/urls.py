5b95b89667ce51991648ff58fa671f9709176b96
Aleya Kamal <aleya.aqilah@gmail.com>
2023-12-04 20:52:05 +0000

    registration form update

--- api/urls.py
+++ api/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.http import HttpResponse
 
-from .views import main_spa
+from .views import main_spa, SignUpView, LogInView
 
 urlpatterns = [
     path('', main_spa),
+    path('signup/', SignUpView, name='signup'),
+    path('login/', LogInView, name='login'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,27 @@
 from django.http import HttpResponse, HttpRequest, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from .forms import CustomUserCreationForm
 
 def main_spa(request: HttpRequest) -> HttpResponse:
+    show_signup_section = True
+
+    if not show_signup_section:
+        return redirect('login')
+
+    return render(request, 'api/spa/index.html', {'show_signup_section': show_signup_section})
+
+def SignUpView(request):
+    form = CustomUserCreationForm()
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+
+    context = {'form':form}
+    return render(request, 'api/spa/index.html', context)
+
+def LogInView(request):
     return render(request, 'api/spa/index.html', {})
