5aa276677e9883e77cc3a06b8a744b20ea5db5e8
Aleya Kamal <aleya.aqilah@gmail.com>
2023-12-21 18:07:16 +0000

    category and articles models

--- api/admin.py
+++ api/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Profile,  Product, Message, MessageResponse
+from .models import User, Profile, Category, Article
 
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Profile)
-admin.site.register(Product)
-admin.site.register(Message)
-admin.site.register(MessageResponse)
+admin.site.register(Category)
+admin.site.register(Article)
+# admin.site.register(Message)
+# admin.site.register(MessageResponse)
 
